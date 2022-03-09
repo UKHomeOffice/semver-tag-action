d8942c8c95ecba30d277c93810f3e9ead280c40f
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-03-09 23:31:04 +0000

    Testing - to revert.

--- dist/index.js
+++ dist/index.js
@@ -11532,25 +11532,10 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438)
 const semver = __nccwpck_require__(1383)
 
-async function createTag(incrementedTag) {
-    const token = core.getInput('github_token', { required: true })
-    const octokit = github.getOctokit(token)
-    const sha = github.context.sha
-    const ref = `refs/tags/${incrementedTag}`
-    await octokit.rest.git.createRef({
-        ...github.context.repo,
-        ref,
-        sha
-    })
-}
-
-async function getLatestTag() {
+async function getLatestTag(octokit, { context } = github) {
     console.log('Getting Latest Tag from Repo')
-    const token = core.getInput('github_token', { required: true })
-    const octokit = github.getOctokit(token)
-
     const { data: refs } = await octokit.rest.git.listMatchingRefs({
-        ...github.context.repo,
+        ...context.repo,
         namespace: 'tags/'
     })
 
@@ -11563,20 +11548,33 @@ async function getLatestTag() {
     return versions[0] || semver.parse('0.0.0')
 }
 
-async function getNewTag(latestTag, increment) {
-    console.log(`Incrementing latest tag "${latestTag.toString()}"`)
+async function calculateNewTag(latestTag, increment) {
+    console.log(`Incrementing latest tag "${latestTag.toString()}" by ${increment}`)
     const newTag = semver.inc(latestTag, increment).toString();
     console.log(`Calculated new tag "${newTag}"`)
     return newTag
 }
 
+async function createTag(incrementedTag, octokit, { context } = github) {
+    const sha = context.sha
+    const ref = `refs/tags/${incrementedTag}`
+    await octokit.rest.git.createRef({
+        ...context.repo,
+        ref,
+        sha
+    })
+}
+
 async function run() {
     try {
+        const token = core.getInput('github_token', { required: true })
+        const octokit = github.getOctokit(token)
+
         const increment = core.getInput('increment', {required: true})
-        const latestTag = await getLatestTag()
-        const newTag = await getNewTag(latestTag, increment)
+        const latestTag = await getLatestTag(octokit)
+        const newTag = await calculateNewTag(latestTag, increment)
+        await createTag(newTag, octokit)
         core.setOutput('version', newTag)
-        await createTag(newTag)
     } catch (error) {
         core.setFailed(error.message)
     }
