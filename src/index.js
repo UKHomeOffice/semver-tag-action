b678c29f105bab676d8bbd3d9d98a49dc3617329
Liam MacPherson <liammacpherson1996@gmail.com>
2022-03-25 21:40:22 +0000

    HOCS-4821: handle hotfix functionality
    At present the action only supports patch, minor and major. This change
    allows for an `hotfix` label which retrieves the repo tag for the base
    sha and increments by the patch level. If the hotfixed tag already
    exists on the branch - the job fails.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 const core = require("@actions/core");
-
-const { getTags, createTag, getOctoKit } = require("../src/github");
+const {
+  getTagsForRepo,
+  createTag,
+  getOctoKit,
+  getHeadRefSha,
+  getTagByCommitSha,
+  repoHasTag,
+} = require("../src/github");
 const { calculateNewTag, getLatestTag } = require("../src/semver");
 
-
 async function run() {
-    try {
-        const token = core.getInput('github_token', { required: true })
-        const octokit = getOctoKit(token)
+  try {
+    const token = core.getInput("github_token", { required: true });
+    const increment = core.getInput("increment", { required: true });
+    const octokit = getOctoKit(token);
 
-        const increment = core.getInput('increment', {required: true})
-        const tags = await getTags(octokit)
+    const repoTags = await getTagsForRepo(octokit);
+    const newTag =
+      increment === "hotfix"
+        ? generateHotfixTag(repoTags)
+        : generateSemverTag(repoTags, increment);
 
-        const latestTag = getLatestTag(tags)
-        const newTag = calculateNewTag(latestTag, increment)
-
-        await createTag(newTag, octokit)
-        core.setOutput('version', newTag)
-    } catch (error) {
-        core.setFailed(error.message)
+    if (!newTag) {
+      core.setFailed("No new tag could be created.");
+      return;
     }
+    core.info(`Creating tag ${newTag}`);
+
+    await createTag(newTag, octokit);
+    core.setOutput("version", newTag);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function generateHotfixTag(tags) {
+  const headSha = getHeadRefSha();
+  const headSemver = getTagByCommitSha(tags, headSha);
+  if (!headSemver) {
+    core.setFailed(`No tag found on repository for SHA: ${headSha}`);
+    return;
+  }
+  core.info(`Found tag ${headSemver} for head SHA ${headSha}`);
+
+  const newPatchTag = calculateNewTag(headSemver, "patch");
+  core.info(`Checking for new patch value: ${newPatchTag}`);
+
+  if (repoHasTag(tags, newPatchTag)) {
+    core.setFailed(`Tag ${newPatchTag} already exists on repository`);
+    return;
+  }
+
+  return newPatchTag;
+}
+
+function generateSemverTag(tags, increment) {
+  const latestTag = getLatestTag(tags);
+  core.info(`Latest repo tag: ${latestTag}`);
+  return calculateNewTag(latestTag, increment);
 }
 
 run();
